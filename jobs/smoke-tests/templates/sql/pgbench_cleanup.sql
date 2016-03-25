aa844f22eeac87077e916abe3576d1d1102bec2e
Thomas Mitchell <tmitchell@starkandwayne.com>
2016-03-25 09:22:27 -0400

    Quoting olympics avoided

---
+++ jobs/smoke-tests/templates/sql/pgbench_cleanup.sql
@@ -0,0 +1,20 @@
+DO $$
+	DECLARE
+		row pg_tables%ROWTYPE;
+	BEGIN
+		FOR row IN 
+			SELECT
+				schemaname,
+				tablename
+			FROM
+				pg_catalog.pg_tables
+			WHERE
+				tableowner = '<%= p('postgres.smoke-tests.target.username') %>'
+			AND
+				tablename LIKE 'pgbench\_%'
+		LOOP
+			EXECUTE 'DROP TABLE ' || quote_ident(row.schemaname) || '.' || quote_ident(row.tablename);
+			RAISE INFO 'Dropped table: %', quote_ident(row.schemaname) || '.' || quote_ident(row.tablename);
+		END LOOP;
+	END $$
+LANGUAGE plpgsql;
